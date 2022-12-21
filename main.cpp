7c70193053a3a254fe202dc8a8fa44bdeb7d42c1
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-12-21 21:40:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+double f ( double x, double &fx){
+fx=pow(x, 2)*cos(x) + 1;
+    return fx;
+}
+
 int main() {
-   return 0;
+double a=0, b=0,err=0, x=0, fx=0;
+
+do {
+    cout << "inserire valori"<< endl;
+    cin >> a >> b ;
+} while (f(a, fx)*f(b, fx)>=0);
+
+do {
+    if ( f(x, fx)!=0) {
+        x = (a + b) / 2;
+        if (f(x, fx) * f(b, fx) < 0) {
+            a = x;
+        } else {
+            b = x;
+        }
+        err = abs((b - a) / 2);
+    }
+} while ( err >= 0.000001);
+
+x=x*10000;
+x=(int)x;
+x=x/10000;
+
+cout << x << endl;
+
+return 0;
 }
